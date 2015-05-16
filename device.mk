ba8783ff4fd019eee6b2a1b1adc629e1ee54b05a
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-05-16 08:14:09 +0800

    hm2014811: Split out power config
    * To be used for recovery too.
    
    Change-Id: Ie5069eca64f07fc62bf2bcbf1d4e0b96984ad416

--- device.mk
+++ device.mk
@@ -142,8 +142,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.rc \
+    init.qcom.power.rc \
     init.qcom.usb.rc \
-    init.recovery.qcom.rc \
     ueventd.qcom.rc
 
 # Wifi
