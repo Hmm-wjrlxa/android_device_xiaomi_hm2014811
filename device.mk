4600865f761b111b8eceabf87d05ba4f6b1b21df
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-05-20 15:43:03 +0800

    hm2014811: Remove unnecessary wifi config
    Change-Id: I773c98f0fa8efd0cd3eaf99973517899fd6909a1

--- device.mk
+++ device.mk
@@ -188,8 +188,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hostapd.conf:system/etc/hostapd/hostapd_default.conf \
     $(LOCAL_PATH)/configs/hostapd.deny:system/etc/hostapd/hostapd.deny \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
