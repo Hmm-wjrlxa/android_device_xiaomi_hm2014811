6aa1adf7dd7860e307f7d88ee954b6ad1f74adef
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-05-25 13:10:24 +0800

    hm2014811: Clean up init services
    Change-Id: I70b26624fe8b65edc89224cbffe871f8f2571489

--- device.mk
+++ device.mk
@@ -162,10 +162,6 @@ PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
     init.qcom.coex.sh \
     init.qcom.fm.sh \
-    init.qcom.audio.sh \
-    init.qcom.efs.sync.sh \
-    init.qcom.modem_links.sh \
-    init.qcom.wifi.sh
 
 PRODUCT_PACKAGES += \
     fstab.qcom \
@@ -173,12 +169,6 @@ PRODUCT_PACKAGES += \
     init.qcom.power.rc \
     init.qcom.usb.rc \
     ueventd.qcom.rc \
-    init.mdm.sh \
-    init.qcom.early_boot.sh \
-    init.qcom.factory.sh \
-    init.qcom.ssr.sh \
-    init.qcom.syspart_fixup.sh \
-    init.qcom.usb.sh
 
 # Wifi
 PRODUCT_PACKAGES += \
